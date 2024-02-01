477dbe602b161d31cdbdfa710de2098dc5630a0b
LAPTOP-HQQQEHNK\user <carossa.luca@iiscellini.it>
2024-02-01 21:59:45 +0100

    iniziato codice js; minuto 6

---
+++ DinoChrome.js
@@ -0,0 +1,24 @@
+//questo è il codice js
+
+let board; 					//let serve è come un id; definisce l'area di lavoro.
+let boardWidht= 750;
+let boardHeight= 250;
+let context;
+
+let dino;
+dinoWidth= 88;
+dinoHeight= 94;
+let dinoX= 50;
+let dinoY= borderHeight - dihnoHeight;
+let dinoImg;
+
+let dino={
+	X:dinoX,
+	Y:dinoY,
+	Width:dinoWidth,
+	Height:dinoHeight,
+}
+
+window.onload = function(){				//permette la personalizzazione di css; grazie a lui possiamo inserire elementi al momento giusto
+
+}
